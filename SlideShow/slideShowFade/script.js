725e7a73b4f55a63b9593a477060da98f2a805ad
hilla10 <hillaman592@gmail.com>
2024-02-25 17:35:44 -1100

    Slide Images

--- SlideShow/slideShowFade/script.js
+++ SlideShow/slideShowFade/script.js
@@ -4,50 +4,48 @@
 
     let currentImage = 0;
 
-    const slideImages = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+    const slideImage = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
 
-    const container = document.getElementById('content');
-    const next = document.getElementById('next');
-    const previous = document.getElementById('previous');
-    const slide = document.getElementById('slide');
-
-    // click event for next button
+    const container = document.querySelector('#content');
+    const slide = document.querySelector('#slide');
+    const previous = document.querySelector('#previous');
+    const next = document.querySelector('#next');
 
     next.addEventListener('click', function (evt) {
         evt.preventDefault;
         currentImage++;
 
-        if (currentImage > slideImages.length - 1) {
+        if (currentImage > slideImage.length - 1) {
             currentImage = 0;
         }
 
-        updateSlide();
+        slider();
     });
 
-    // click event for previous button
 
     previous.addEventListener('click', function (evt) {
         evt.preventDefault;
         currentImage--;
 
         if (currentImage < 0) {
-            currentImage = slideImages.length - 1;
+            currentImage = slideImage.length - 1;
         }
 
-        updateSlide();
+        slider();
     });
 
-    // Update the slide img
+    function slider() {
 
-    function updateSlide() {
+
         const newSlide = document.createElement('img');
-        newSlide.src = `images/${slideImages[currentImage]}`;
+        newSlide.classList = 'fadeInImg';
         container.appendChild(newSlide);
-        newSlide.className = 'fadeInImg';
+        newSlide.src = `images/${slideImage[currentImage]}`;
 
         if (container.children.length > 2) {
             container.removeChild(container.children[0]);
         }
     }
+
 
 }());
